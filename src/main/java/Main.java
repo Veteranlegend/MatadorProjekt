1674d71e04c89bdf69090fbbeab761767fc237ca
Ahmad El Hag <s210030@student.dtu.dk>
2023-01-17 21:17:55 +0100

    Tilføjet main klasse

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import Controler.Spil;
+import gui_fields.GUI_Car;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Color color = Color.ORANGE;
+        //System.out.println(String.valueOf(color));
+
+        //System.out.println(GUI_Car.Type.UFO.toString());
+        Spil spil = new Spil();
+        spil.play();
+    }
+
+}
