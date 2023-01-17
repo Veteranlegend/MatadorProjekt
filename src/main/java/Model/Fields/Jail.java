57dd0084d03b0ef61f8bd5b3feed5f76a8bcf66c
msbeihi <s210085@student.dtu.dk>
2023-01-17 21:38:38 +0100

    Lavet abstrakt felter til ownable samt fængsel

---
+++ src/main/java/Model/Fields/Jail.java
@@ -0,0 +1,13 @@
+package Model.Fields;
+
+public class Jail extends Field {
+    /**
+     *
+     * @param name
+     * @param message
+     */
+    public Jail(String name, String message) {
+        super(name, message);
+    }
+
+}
