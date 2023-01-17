d3df07c23590e729ef438bca9d0f943683f71777
ahsunalla.patan@gmail.com <ahsunalla.patan@gmail.com>
2023-01-17 21:54:03 +0100

    movefield amouunt rettet

--- src/main/java/Model/Chance/MoveFieldAmount.java
+++ src/main/java/Model/Chance/MoveFieldAmount.java
@@ -6,12 +6,19 @@ import gui_main.GUI;
 import View.ViewGUI;
 
 public class MoveFieldAmount extends ChanceCard {
-    public MoveFieldAmount(String description) {
+
+    public int fieldAmount;
+
+    public MoveFieldAmount(String description, int fieldAmount) {
         super(description);
+        this.fieldAmount = fieldAmount;
     }
 
     @Override
     public void doCard(Spiller player, ViewGUI gui) {
         super.doCard(player, gui);
+        player.setPosition(player.getPosition() + fieldAmount);
+        gui.moveCarToField(player, player.getPosition());
     }
+
 }
